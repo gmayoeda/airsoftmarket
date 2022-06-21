0ebeeba5c0f7638a79ed5f8e1027217bad224a5b
mac <mac@macs-MacBook-Pro.local>
2022-06-21 11:13:20 +0700

    final

--- lib/app/widget/btn_loading.dart
+++ lib/app/widget/btn_loading.dart
@@ -7,27 +7,32 @@ class BtnLoading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 45,
-      decoration: BoxDecoration(
-        color: Colors.grey[400],
-        borderRadius: BorderRadius.all(
-          Radius.circular(15),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Container(
+        width: double.infinity,
+        height: 45,
+        decoration: BoxDecoration(
+          color: Colors.grey[400],
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
+          ),
         ),
-      ),
-      alignment: Alignment.center,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircularProgressIndicator(color: Colors.white),
-          SizedBox(width: 10),
-          Text(
-            " LOADING",
-            style: TextStyle(
-                fontSize: 17, color: Colors.white, fontWeight: FontWeight.w600),
-          )
-        ],
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(color: Colors.white),
+            SizedBox(width: 10),
+            Text(
+              " LOADING",
+              style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600),
+            )
+          ],
+        ),
       ),
     );
   }
